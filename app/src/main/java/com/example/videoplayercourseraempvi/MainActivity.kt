62257e79a22d2d5b009907090bbe83fa24928b83
vir-th-w11 <gruningzen202302@gmail.com>
2023-09-19 18:57:50 -0300

    feat:apply string template to simplify code

--- app/src/main/java/com/example/videoplayercourseraempvi/MainActivity.kt
+++ app/src/main/java/com/example/videoplayercourseraempvi/MainActivity.kt
@@ -6,27 +6,17 @@ import android.os.Bundle
 import android.widget.MediaController
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.ui.AppBarConfiguration
-//import com.example.videoplayercourseraempvi.databinding.ActivityMainBinding
-
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    //private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-        //setContentView(android.R.layout.main_activity)
 
         val videoView = findViewById<VideoView>(R.id.testView)
         val mediaController = MediaController(this)
         mediaController.setAnchorView(videoView)
 
-        val uri:Uri = parse(
-            "android.resource://" + packageName + "/raw/tensor"
-        )
+        val uri:Uri = parse("android.resource://$packageName/raw/tensor")
         videoView.setMediaController(mediaController)
         videoView.setVideoURI(uri)
         videoView.requestFocus()
